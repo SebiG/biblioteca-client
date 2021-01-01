c4892c939fb90799b25b0c109355eea1e49a08f7
Sebastian Deaconu <sebastian.deaconu@ecenta.com>
2021-01-01 17:35:43 +0200

    table for all books

--- src/controllers/UserView.java
+++ src/controllers/UserView.java
@@ -2,30 +2,65 @@ package controllers;
 
 import java.util.List;
 
+//import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import application.ConnectionSingleton;
-import application.HelperClass;
+import application.H;
+//import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import models.Book;
 
 public class UserView {
+	
+    @FXML
+    private TableView<Book> listOfBooksTable;
+
+    @FXML
+    private TableColumn<Book, String> bookNameCol;
+
+    @FXML
+    private TableColumn<Book, String> authorCol;
+
+    @FXML
+    private TableColumn<Book, Integer> stockCol;
+
+    @FXML
+    private TableColumn<Book, Button> actionCol;
+
 	@FXML
-	private Label menuLabel;
-	
-	
-	
-	@FXML
-	void initialize() {
-		System.out.println("User interface is running!");
-		JsonObject obj = HelperClass.buildJsonObj(List.of("filter", "all"));
-		JsonObject serverResponse = ConnectionSingleton.getInstance().get("getBooks", obj);
-	}
-
-
-
-	private void buildJsonObj(List<String> of) {
-		// TODO Auto-generated method stub
+	void initialize() throws Exception {
+		H.puts("User interface is running!");
+		JsonObject obj = H.buildJsonObj(List.of("filter", "all"));
+		JsonArray serverResponse = ConnectionSingleton.getInstance().gets("getBooks", obj);
 		
+		if(serverResponse == null) {
+			throw new Exception("Null server response error!");
+		}
+		H.puts(serverResponse.toString());
+		initBookTable(H.toObservableList(serverResponse, Book.class));
 	}
+
+	private void initBookTable(ObservableList<Book> observableList) {
+		H.puts(observableList.toString());
+		initBookTableCells();
+		listOfBooksTable.setItems(observableList);
+	}
+
+
+	private void initBookTableCells() {
+		bookNameCol.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+		authorCol.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
+		stockCol.setCellValueFactory(new PropertyValueFactory<Book, Integer>("stock"));
+		actionCol.setCellValueFactory(new PropertyValueFactory<Book, Button>("requestBtn"));
+	}
+
+	
+    // Observable list of books	
 }
